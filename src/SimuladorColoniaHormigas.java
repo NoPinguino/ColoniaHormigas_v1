d2d865f6edf707d215ae11133f8a02c68c46b641
NoPinguino <misael.delamorena@gmail.com>
2025-10-30 15:11:59 +0100

    Simulador colonia hormigas.

--- src/SimuladorColoniaHormigas.java
+++ src/SimuladorColoniaHormigas.java
@@ -27,7 +27,7 @@ public class SimuladorColoniaHormigas {
 
     /**
      * Creo tantas hormigas obreras como me indique el atributo NUMERO_OBRERAS
-     * Big O(NUMERO_OBRERAS * ANCHO * ALTO)
+     * Big O(?) no estoy seguro de la complejidad, ya que depende de números aleatorios.
      */
     public void generarHormigaObrera() {
         int hormigas_agregadas = 0;
@@ -47,52 +47,112 @@ public class SimuladorColoniaHormigas {
     }
 
     /**
-     *
+     * Ejecuta la simulación, dejando un intervalo entre ejecuciones/iteraciones. Control de errores en Thread.sleep
+     * Big O(infinito) no se detiene, es un bucle infinito
      */
     public void ejecutar() {
-
+        generarHormigaObrera();
+        this.simulacionActiva = true;
+        while (this.simulacionActiva) {
+            actualizarVisualizacion();
+            try {
+                Thread.sleep(INTERVALO_ACTUALIZACION);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 
     /**
-     *
+     * Detiene la simulación y los hilos de las hormigas.
+     * Big O(n) n=cantidad de hormigas
      */
     public void detenerSimulacion() {
-
+        this.simulacionActiva = false;
+        for (Hormiga hormiga : hormigas.values()) {
+            hormiga.detener();
+        }
     }
 
     /**
-     *
+     * Imprime el estado actual del mapa, actualizando primero el propio mapa.
+     * Big O(n) n=cantidad de hormigas
      */
     private void actualizarVisualizacion() {
-
+        limpiarConsola();
+        this.mapa.prepararMapa(this.hormigas); //repararMapa() recibe String->id_hormiga y Hormiga->objHormigaObrera
+        this.mapa.mostrarMapa();
     }
 
     /**
-     *
+     * Método que recorre el mapa hormigas, pasando cada hormiga indivialmente a moverHormigaAleatoriamente()
+     * Big O(n)
      */
     private synchronized void moverTodasLasHormigas() {
-
+        for (Hormiga hormiga : this.hormigas.values()) {
+            moverHormigaAleatoriamente(hormiga);
+        }
     }
 
     /**
-     *
-     * @param hormiga
+     * Método que cambia los valores de Posicion de una instancia de la clase hormiga.
+     * Big O(?) depende de números aleatorios, no para hasta generar una posición válida.
+     * @param hormiga Instancia de la clase hormiga
      */
     private synchronized void moverHormigaAleatoriamente(Hormiga hormiga) {
+        boolean movimiento_posible = false;
+        while (!movimiento_posible) {
+            int direccion = this.random.nextInt(4); //Genera un número entre 0-3
+            int deltaX = DIRECCIONES[direccion][0]; // En DIRECCIONES[][] el elemento[n][0] corresponde a movimiento en eje Y
+            int deltaY = DIRECCIONES[direccion][1]; // En DIRECCIONES[][] el elemento[n][1] corresponde a movimiento en eje X
+
+            Posicion posicion = hormiga.getPosicion();
+            Posicion nuevaPosicion = posicion.mover(deltaX, deltaY);
+
+            /*
+            Comprueba que las coordenadas se encuentren en los límites del tablero llamando a dentroLimites()
+            Compara de forma individual los valores X y los valores Y de hormiguero y nueva posición, si al
+            menos uno de esos dos valores es distinto al otro, las posiciones de hormiguero y hormiga son distintas.
+            Si ambas comparaciones devuelven true, la hormiga se puede mover, si no, lo vuelve a intentar en la
+            próxima iteración de while().
+             */
+            if (this.mapa.dentroLimites(nuevaPosicion) && (this.mapa.getHormiguero().getX() != nuevaPosicion.getX() || this.mapa.getHormiguero().getY() != nuevaPosicion.getY())) {
+                hormiga.setPosicion(nuevaPosicion);
+                movimiento_posible = true;
+            }
+        }
 
     }
 
     /**
-     *
+     * Este método imprime 10 saltos e linea, ayudando a leer fácilmente la terminal.
+     * Big O(n) n=cantidad de lineas en blanco
      */
     private void limpiarConsola() {
-
+        for (int i = 0; i < 10; i++) {
+            System.out.println();
+        }
     }
 
     /**
-     *
+     * Método que imprime las estadisticas de la colonia de hormigas.
      */
     private void mostrarEstadisticas() {
+        int hormigas_activas = 0;
+        for (Hormiga hormiga : hormigas.values()) {
+            if (hormiga.isActiva()) hormigas_activas++;
+        }
+        int hormigas_obreras = 0;
+        for (Hormiga hormiga : hormigas.values()) {
+            if (hormiga.getTipo() == TipoHormiga.OBRERA) hormigas_obreras++;
+        }
 
+        System.out.println("======================================");
+        System.out.println("Estadísticas de la simulación: ");
+        System.out.println("======================================");
+        System.out.println("Hormigas activas: " + hormigas_activas);
+        System.out.println("Hormigas obreras: " + hormigas_obreras);
+        System.out.println("Posición hormiguero: " + mapa.getHormiguero().getX() + " " + mapa.getHormiguero().getY() + ".");
+        System.out.println("Intervalo entre ejecuciones: " + INTERVALO_ACTUALIZACION + "ms.");
     }
 }
